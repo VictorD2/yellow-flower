8611a2a666081bd98ed55330b7eff8d7b5041566
Victor Hernandez <victor-2027@hotmail.com>
2023-09-21 02:37:07 -0500

    ended

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: "#225679",
+      secondary: "#33435A",
+      transparent: "transparent",
+      background: "#eaf3f147",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      rose: colors.rose,
+      amber: colors.amber,
+      cyan: colors.cyan,
+      emerald: colors.emerald,
+      fuchsia: colors.fuchsia,
+      lime: colors.lime,
+      orange: colors.orange,
+      pink: colors.pink,
+      purple: colors.purple,
+      sky: colors.sky,
+      slate: colors.slate,
+      stone: colors.stone,
+      teal: colors.teal,
+      violet: colors.violet,
+      zinc: colors.zinc,
     },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
